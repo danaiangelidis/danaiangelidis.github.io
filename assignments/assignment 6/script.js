881d7d36fa3118662b93e1683f82507712f82df3
danaiangelidis <angelidisdanai@gmail.com>
2023-09-17 23:14:32 -0400

    added assignment 6

--- assignments/assignment 6/script.js
+++ assignments/assignment 6/script.js
@@ -10,8 +10,23 @@ const animate = () => {
     document.getElementById("dancing-cat").classList.add("move");
 }
 
+const createDiv = () => {
+    const name = document.getElementById("txt-product-name").value;
+    const comment = document.getElementById("txt-comment").value;
+    const rating = document.getElementById("txt-rating").value;
+    const username = document.getElementById("txt-user-name").value;
+    console.log(name + " " + comment + " " + rating + " " + username);
+
+    const newDiv = document.createElement("div");
+
+    newDiv.innerHTML = "<h1>" + name + "</h1>" + comment + "<br><br>" + rating + "/5 stars<br><br> by " + username;
+    newDiv.classList.add("comment");
+    document.getElementById("inputs").appendChild(newDiv);
+};
+
 window.onload = () => {
     document.getElementById("show").onclick = showCat;
     document.getElementById("hide").onclick = hideCat;
     document.getElementById("animate-button").onclick = animate;
+    document.getElementById("submit-button").onclick = createDiv;
 };
