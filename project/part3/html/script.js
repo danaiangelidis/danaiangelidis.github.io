96c080786a362f6e3134a6fc90f48835ac9b5bac
danaiangelidis <angelidisdanai@gmail.com>
2023-10-13 16:00:33 -0400

    fixed narrow view nav

--- project/part3/html/script.js
+++
@@ -1,7 +0,0 @@
-const toggleNav = () => {
-    document.getElementById("nav-in-nav-b").classList.toggle("hide");
-}
-
-window.onload = () => {
-    document.getElementById("burger").onclick = toggleNav;
-}
